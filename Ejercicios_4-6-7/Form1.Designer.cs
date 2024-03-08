5f31b31ce0024be41253254ee7f30b2ef742e273
pc <pc@LENNY>
2024-03-08 10:11:59 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicios_4-6-7/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Ejercicios_4_6_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_prueva = new System.Windows.Forms.Label();
+            this.lbl_estado = new System.Windows.Forms.Label();
+            this.lblNota2 = new System.Windows.Forms.Label();
+            this.lbl_nota = new System.Windows.Forms.Label();
+            this.lbl_promedio1 = new System.Windows.Forms.Label();
+            this.lblNota4 = new System.Windows.Forms.Label();
+            this.lblNota3 = new System.Windows.Forms.Label();
+            this.lbl_estado2 = new System.Windows.Forms.Label();
+            this.lbl_Total = new System.Windows.Forms.Label();
+            this.lblNota1 = new System.Windows.Forms.Label();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_calcular_promedio = new System.Windows.Forms.Button();
+            this.btn_cerrar = new System.Windows.Forms.Button();
+            this.txtNotaCompletiva = new System.Windows.Forms.TextBox();
+            this.txtResultadoCompletivo = new System.Windows.Forms.TextBox();
+            this.txtTotalCompletivo = new System.Windows.Forms.TextBox();
+            this.txtPruebaCompletiva = new System.Windows.Forms.TextBox();
+            this.txt_estado = new System.Windows.Forms.TextBox();
+            this.txt_4 = new System.Windows.Forms.TextBox();
+            this.txt_3 = new System.Windows.Forms.TextBox();
+            this.txt_2 = new System.Windows.Forms.TextBox();
+            this.txt_1 = new System.Windows.Forms.TextBox();
+            this.lbl_promedio = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_prueva
+            // 
+            this.lbl_prueva.AutoSize = true;
+            this.lbl_prueva.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_prueva.Location = new System.Drawing.Point(320, 75);
+            this.lbl_prueva.Name = "lbl_prueva";
+            this.lbl_prueva.Size = new System.Drawing.Size(150, 19);
+            this.lbl_prueva.TabIndex = 0;
+            this.lbl_prueva.Text = "Prueva completiva";
+            // 
+            // lbl_estado
+            // 
+            this.lbl_estado.AutoSize = true;
+            this.lbl_estado.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_estado.Location = new System.Drawing.Point(75, 259);
+            this.lbl_estado.Name = "lbl_estado";
+            this.lbl_estado.Size = new System.Drawing.Size(60, 19);
+            this.lbl_estado.TabIndex = 1;
+            this.lbl_estado.Text = "Estado";
+            // 
+            // lblNota2
+            // 
+            this.lblNota2.AutoSize = true;
+            this.lblNota2.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota2.Location = new System.Drawing.Point(75, 95);
+            this.lblNota2.Name = "lblNota2";
+            this.lblNota2.Size = new System.Drawing.Size(56, 19);
+            this.lblNota2.TabIndex = 2;
+            this.lblNota2.Text = "Nota2";
+            // 
+            // lbl_nota
+            // 
+            this.lbl_nota.AutoSize = true;
+            this.lbl_nota.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nota.Location = new System.Drawing.Point(344, 101);
+            this.lbl_nota.Name = "lbl_nota";
+            this.lbl_nota.Size = new System.Drawing.Size(126, 19);
+            this.lbl_nota.TabIndex = 3;
+            this.lbl_nota.Text = "Nota completia";
+            // 
+            // lbl_promedio1
+            // 
+            this.lbl_promedio1.AutoSize = true;
+            this.lbl_promedio1.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_promedio1.Location = new System.Drawing.Point(60, 216);
+            this.lbl_promedio1.Name = "lbl_promedio1";
+            this.lbl_promedio1.Size = new System.Drawing.Size(83, 19);
+            this.lbl_promedio1.TabIndex = 4;
+            this.lbl_promedio1.Text = "Promedio";
+            // 
+            // lblNota4
+            // 
+            this.lblNota4.AutoSize = true;
+            this.lblNota4.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota4.Location = new System.Drawing.Point(75, 147);
+            this.lblNota4.Name = "lblNota4";
+            this.lblNota4.Size = new System.Drawing.Size(56, 19);
+            this.lblNota4.TabIndex = 5;
+            this.lblNota4.Text = "Nota4";
+            // 
+            // lblNota3
+            // 
+            this.lblNota3.AutoSize = true;
+            this.lblNota3.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota3.Location = new System.Drawing.Point(75, 121);
+            this.lblNota3.Name = "lblNota3";
+            this.lblNota3.Size = new System.Drawing.Size(56, 19);
+            this.lblNota3.TabIndex = 6;
+            this.lblNota3.Text = "Nota3";
+            // 
+            // lbl_estado2
+            // 
+            this.lbl_estado2.AutoSize = true;
+            this.lbl_estado2.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_estado2.Location = new System.Drawing.Point(410, 185);
+            this.lbl_estado2.Name = "lbl_estado2";
+            this.lbl_estado2.Size = new System.Drawing.Size(60, 19);
+            this.lbl_estado2.TabIndex = 7;
+            this.lbl_estado2.Text = "Estado";
+            // 
+            // lbl_Total
+            // 
+            this.lbl_Total.AutoSize = true;
+            this.lbl_Total.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Total.Location = new System.Drawing.Point(334, 129);
+            this.lbl_Total.Name = "lbl_Total";
+            this.lbl_Total.Size = new System.Drawing.Size(136, 19);
+            this.lbl_Total.TabIndex = 8;
+            this.lbl_Total.Text = "Total completivo";
+            // 
+            // lblNota1
+            // 
+            this.lblNota1.AutoSize = true;
+            this.lblNota1.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota1.Location = new System.Drawing.Point(75, 69);
+            this.lblNota1.Name = "lblNota1";
+            this.lblNota1.Size = new System.Drawing.Size(56, 19);
+            this.lblNota1.TabIndex = 9;
+            this.lblNota1.Text = "Nota1";
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.BackColor = System.Drawing.Color.Lime;
+            this.btn_calcular.Location = new System.Drawing.Point(135, 182);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(100, 24);
+            this.btn_calcular.TabIndex = 10;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = false;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.BackColor = System.Drawing.Color.Yellow;
+            this.btn_limpiar.Location = new System.Drawing.Point(618, 46);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(100, 24);
+            this.btn_limpiar.TabIndex = 11;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = false;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_calcular_promedio
+            // 
+            this.btn_calcular_promedio.BackColor = System.Drawing.Color.Lime;
+            this.btn_calcular_promedio.Location = new System.Drawing.Point(423, 229);
+            this.btn_calcular_promedio.Name = "btn_calcular_promedio";
+            this.btn_calcular_promedio.Size = new System.Drawing.Size(118, 23);
+            this.btn_calcular_promedio.TabIndex = 12;
+            this.btn_calcular_promedio.Text = "Calcular promedio";
+            this.btn_calcular_promedio.UseVisualStyleBackColor = false;
+            this.btn_calcular_promedio.Click += new System.EventHandler(this.btn_calcular_promedio_Click);
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.BackColor = System.Drawing.Color.Red;
+            this.btn_cerrar.Location = new System.Drawing.Point(618, 90);
+            this.btn_cerrar.Name = "btn_cerrar";
+            this.btn_cerrar.Size = new System.Drawing.Size(100, 24);
+            this.btn_cerrar.TabIndex = 13;
+            this.btn_cerrar.Text = "Cerrar";
+            this.btn_cerrar.UseVisualStyleBackColor = false;
+            this.btn_cerrar.Click += new System.EventHandler(this.btn_cerrar_Click);
+            // 
+            // txtNotaCompletiva
+            // 
+            this.txtNotaCompletiva.Location = new System.Drawing.Point(475, 102);
+            this.txtNotaCompletiva.Name = "txtNotaCompletiva";
+            this.txtNotaCompletiva.Size = new System.Drawing.Size(100, 20);
+            this.txtNotaCompletiva.TabIndex = 14;
+            // 
+            // txtResultadoCompletivo
+            // 
+            this.txtResultadoCompletivo.Location = new System.Drawing.Point(475, 185);
+            this.txtResultadoCompletivo.Name = "txtResultadoCompletivo";
+            this.txtResultadoCompletivo.Size = new System.Drawing.Size(100, 20);
+            this.txtResultadoCompletivo.TabIndex = 15;
+            // 
+            // txtTotalCompletivo
+            // 
+            this.txtTotalCompletivo.Location = new System.Drawing.Point(475, 128);
+            this.txtTotalCompletivo.Name = "txtTotalCompletivo";
+            this.txtTotalCompletivo.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalCompletivo.TabIndex = 16;
+            // 
+            // txtPruebaCompletiva
+            // 
+            this.txtPruebaCompletiva.Location = new System.Drawing.Point(475, 76);
+            this.txtPruebaCompletiva.Name = "txtPruebaCompletiva";
+            this.txtPruebaCompletiva.Size = new System.Drawing.Size(100, 20);
+            this.txtPruebaCompletiva.TabIndex = 17;
+            // 
+            // txt_estado
+            // 
+            this.txt_estado.Location = new System.Drawing.Point(135, 259);
+            this.txt_estado.Name = "txt_estado";
+            this.txt_estado.Size = new System.Drawing.Size(100, 20);
+            this.txt_estado.TabIndex = 18;
+            // 
+            // txt_4
+            // 
+            this.txt_4.Location = new System.Drawing.Point(135, 147);
+            this.txt_4.Name = "txt_4";
+            this.txt_4.Size = new System.Drawing.Size(100, 20);
+            this.txt_4.TabIndex = 19;
+            // 
+            // txt_3
+            // 
+            this.txt_3.Location = new System.Drawing.Point(135, 121);
+            this.txt_3.Name = "txt_3";
+            this.txt_3.Size = new System.Drawing.Size(100, 20);
+            this.txt_3.TabIndex = 20;
+            // 
+            // txt_2
+            // 
+            this.txt_2.Location = new System.Drawing.Point(135, 95);
+            this.txt_2.Name = "txt_2";
+            this.txt_2.Size = new System.Drawing.Size(100, 20);
+            this.txt_2.TabIndex = 21;
+            // 
+            // txt_1
+            // 
+            this.txt_1.Location = new System.Drawing.Point(135, 69);
+            this.txt_1.Name = "txt_1";
+            this.txt_1.Size = new System.Drawing.Size(100, 20);
+            this.txt_1.TabIndex = 22;
+            // 
+            // lbl_promedio
+            // 
+            this.lbl_promedio.AutoSize = true;
+            this.lbl_promedio.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_promedio.Location = new System.Drawing.Point(164, 216);
+            this.lbl_promedio.Name = "lbl_promedio";
+            this.lbl_promedio.Size = new System.Drawing.Size(27, 19);
+            this.lbl_promedio.TabIndex = 23;
+            this.lbl_promedio.Text = "lol";
+            this.lbl_promedio.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(732, 305);
+            this.Controls.Add(this.lbl_promedio);
+            this.Controls.Add(this.txt_1);
+            this.Controls.Add(this.txt_2);
+            this.Controls.Add(this.txt_3);
+            this.Controls.Add(this.txt_4);
+            this.Controls.Add(this.txt_estado);
+            this.Controls.Add(this.txtPruebaCompletiva);
+            this.Controls.Add(this.txtTotalCompletivo);
+            this.Controls.Add(this.txtResultadoCompletivo);
+            this.Controls.Add(this.txtNotaCompletiva);
+            this.Controls.Add(this.btn_cerrar);
+            this.Controls.Add(this.btn_calcular_promedio);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.lblNota1);
+            this.Controls.Add(this.lbl_Total);
+            this.Controls.Add(this.lbl_estado2);
+            this.Controls.Add(this.lblNota3);
+            this.Controls.Add(this.lblNota4);
+            this.Controls.Add(this.lbl_promedio1);
+            this.Controls.Add(this.lbl_nota);
+            this.Controls.Add(this.lblNota2);
+            this.Controls.Add(this.lbl_estado);
+            this.Controls.Add(this.lbl_prueva);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_prueva;
+        private System.Windows.Forms.Label lbl_estado;
+        private System.Windows.Forms.Label lblNota2;
+        private System.Windows.Forms.Label lbl_nota;
+        private System.Windows.Forms.Label lbl_promedio1;
+        private System.Windows.Forms.Label lblNota4;
+        private System.Windows.Forms.Label lblNota3;
+        private System.Windows.Forms.Label lbl_estado2;
+        private System.Windows.Forms.Label lbl_Total;
+        private System.Windows.Forms.Label lblNota1;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_calcular_promedio;
+        private System.Windows.Forms.Button btn_cerrar;
+        private System.Windows.Forms.TextBox txtNotaCompletiva;
+        private System.Windows.Forms.TextBox txtResultadoCompletivo;
+        private System.Windows.Forms.TextBox txtTotalCompletivo;
+        private System.Windows.Forms.TextBox txtPruebaCompletiva;
+        private System.Windows.Forms.TextBox txt_estado;
+        private System.Windows.Forms.TextBox txt_4;
+        private System.Windows.Forms.TextBox txt_3;
+        private System.Windows.Forms.TextBox txt_2;
+        private System.Windows.Forms.TextBox txt_1;
+        private System.Windows.Forms.Label lbl_promedio;
+    }
+}
+
---
+++ Ejercicios_4-6-7/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios_4_6_7
+{
+   
+    public partial class Form1 : Form
+    {
+        
+        private int nota1, nota2, nota3, nota4, promedio;
+       
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+
+           
+            lbl_promedio.Text = String.Empty;
+            txt_1.Clear();
+            txt_2.Clear();
+            txt_3.Clear();
+            txt_4.Clear();
+            txt_estado.Clear();
+            txtPruebaCompletiva.Clear();
+            txtNotaCompletiva.Clear();
+            txtTotalCompletivo.Clear();
+            txtResultadoCompletivo.Clear();
+        }
+
+        private void btn_cerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_calcular_promedio_Click(object sender, EventArgs e)
+        {
+
+            PruebaCompletiva = int.Parse(txtPruebaCompletiva.Text);
+            TotalCompletivo = (NotaCompletiva + PruebaCompletiva / 2);
+
+            txtTotalCompletivo.Text = TotalCompletivo.ToString();
+
+            //determinar si aprobo o reprobo la prueba completiva
+            if (TotalCompletivo >= 70)
+            {
+
+                txtResultadoCompletivo.Text = "Aprobado";
+                txtResultadoCompletivo.ForeColor = Color.Green;
+
+
+            }
+
+            else
+
+            {
+                txtResultadoCompletivo.Text = "Reprobado";
+                txtResultadoCompletivo.ForeColor = Color.Red;
+
+            }
+
+        }
+
+        private int NotaCompletiva, TotalCompletivo, PruebaCompletiva;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            txtNotaCompletiva.Visible = false;
+            txtPruebaCompletiva.Visible = false;
+            txtResultadoCompletivo.Visible = false;
+            txtTotalCompletivo.Visible = false;
+            btn_calcular_promedio.Visible = false;
+            lbl_estado2.Visible = false;
+            lbl_prueva.Visible = false;
+            lbl_Total.Visible = false;
+            lbl_nota.Visible = false;
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            txtPruebaCompletiva.Clear();
+            txtTotalCompletivo.Clear();
+           
+            txtResultadoCompletivo.Clear();
+            txtNotaCompletiva.Enabled = false;
+            txtPruebaCompletiva.Enabled = false;
+            txtTotalCompletivo.Enabled = false;
+            txtResultadoCompletivo.Enabled = false;
+            btn_calcular_promedio.Enabled = false;
+
+
+            nota1 = int.Parse(txt_1.Text);
+            nota2 = int.Parse(txt_2.Text);
+            nota3 = int.Parse(txt_3.Text);
+            nota4 = int.Parse(txt_4.Text);
+
+            promedio = (nota1 + nota2 + nota3 + nota4) / 4;
+            NotaCompletiva = promedio / 2;
+
+
+
+
+
+           
+            lbl_promedio.Text = promedio.ToString();
+
+
+            //determinar si el estudiante aprobo o reprobo
+            if (promedio >= 70)
+
+            {
+
+                txt_estado.Text = "Aprobado";
+                txt_estado.ForeColor = Color.Green;
+                txtNotaCompletiva.Clear();
+            }
+
+            else
+            {
+                lbl_estado2.Visible = true;
+                lbl_prueva.Visible = true;
+                lbl_Total.Visible = true;
+                lbl_nota.Visible = true;
+                lbl_promedio.Visible = true;
+                txtNotaCompletiva.Visible = true;
+                txtPruebaCompletiva.Visible = true;
+                txtResultadoCompletivo.Visible = true;
+                txtTotalCompletivo.Visible = true;
+                btn_calcular_promedio.Visible = true;
+
+                txt_estado.Text = "Reprobado";
+                txt_estado.ForeColor = Color.Red;
+                txtPruebaCompletiva.Enabled = true;
+                txtResultadoCompletivo.Enabled = true;
+                btn_calcular_promedio.Enabled = true;
+                txtNotaCompletiva.Text = NotaCompletiva.ToString();
+
+
+
+
+            }
+
+
+
+        }
+    }
+    }
+
